5eda5652ac8a1bebf3c638749268d63448dba794
TheFoxAzubirs <58655177+TheFoxAzubirs@users.noreply.github.com>
2019-12-09 06:49:12 +0700

    Add files via upload

---
+++ bashrcasli.sh
@@ -0,0 +1,4 @@
+cd #
+cd ..
+mv usr/etc/bashrc.asli usr/etc/bash.bashrc
+ls usr/etc/bash*
---
+++ bashrcfox.sh
@@ -0,0 +1,5 @@
+cd #
+cd ..
+mv usr/etc/bash.bashrc usr/etc/bashrc.asli
+cp home/bashrc.fox usr/etc/bash.bashrc
+ls usr/etc/bash*
